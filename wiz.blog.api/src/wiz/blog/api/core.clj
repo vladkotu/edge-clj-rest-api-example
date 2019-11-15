7c386b2fd105bd5b87050c96e03b825221f9c951
Vlad Kotu <vladimir.buhuru@gmail.com>
2019-11-15 18:38:33 +0200

    user env port for bidi main route

--- wiz.blog.api/src/wiz/blog/api/core.clj
+++ wiz.blog.api/src/wiz/blog/api/core.clj
@@ -11,18 +11,3 @@
 (defmethod ig/init-key ::string
   [_ x]
   (string-resource x))
-
-(defmethod ig/init-key ::fcom
-  [_ xs]
-  (string-resource (clojure.string/join "/" xs)))
-
-(defmethod ig/init-key ::fcom-wrapped
-  [_ {:keys [origin wrap-with]}]
-  (let [f          (first origin)
-        l          (last origin)
-        wf         (first wrap-with)
-        wl         (last wrap-with)
-        sub-origin (subvec origin 1 (dec (count origin)))]
-    (conj (apply vector (str wf f) sub-origin) (str l wl))))
-
-
