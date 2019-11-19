24e3a81363931bcb4829119ba1754842ae17428f
Vlad Kotu <vladimir.buhuru@gmail.com>
2019-11-19 21:29:57 +0200

    move sql to separate folder

--- wiz.blog.api/src/wiz/blog/api/db.clj
+++ wiz.blog.api/src/wiz/blog/api/db.clj
@@ -1,38 +1,13 @@
 (ns wiz.blog.api.db
   (:require
    [integrant.core :as ig]
+   [clojure.tools.logging :as log]
    [wiz.blog.api.db.authors :as authors]))
 
 (def db (atom nil))
 
-(comment (authors/insert-authors
-          @db
-          {:authors
-           (map vals
-                [{:name      "Ivan"
-                  :email     "ivan@ttt.com"
-                  :nickname  "ivanko"
-                  :biography nil}
-                 {:name      "Sven"
-                  :email     "svet@ttt.com"
-                  :nickname  "svenlo"
-                  :biography nil}])}))
-
-(comment
-  (authors/get-author-by-email @db {:email "svet@ttt.com"})
-  (let [{:keys [id]} (authors/get-author-by-email @db {:email "svet@ttt.com"})]
-    (authors/generic-update @db {:id id :updates {:name "Svetlana"}})))
-
-(comment
-  (authors/clj-expr-single-sqlvec {:cols ["email" "name"]})
-  (authors/clj-expr-single @db {:cols ["email" "name"] :id 1})
-  (authors/get-author-by-email @db {:email "svet@ttt.com"}))
-
-(comment
-  (authors/get-all-authors @db))
-
 (defmethod ig/init-key ::db-spec
   [_ opts]
-  (println opts)
+  (log/info "Initialize db")
   (reset! db opts))
 
--- wiz.blog.api/src/wiz/blog/api/db/authors.clj
+++ wiz.blog.api/src/wiz/blog/api/db/authors.clj
@@ -2,6 +2,35 @@
   (:require
    [hugsql.core :as hugsql]))
 
-(hugsql/def-db-fns "wiz/blog/api/db/authors.sql")
-(hugsql/def-sqlvec-fns "wiz/blog/api/db/authors.sql")
+(hugsql/def-db-fns "wiz/blog/api/db/sql/authors.sql")
+;; this should be removed in prod mode
+(hugsql/def-sqlvec-fns "wiz/blog/api/db/sql/authors.sql")
+
+(comment (def db (atom {:dbtype "postgresql", :user "vladkotu", :password "pwd123", :dbname "blog_db", :port 54320, :host "localhost"})))
+(comment (authors/insert-authors
+          @db
+          {:authors
+           (map vals
+                [{:name      "Ivan"
+                  :email     "ivan@ttt.com"
+                  :nickname  "ivanko"
+                  :biography nil}
+                 {:name      "Sven"
+                  :email     "svet@ttt.com"
+                  :nickname  "svenlo"
+                  :biography nil}])}))
+
+(comment
+  (authors/get-author-by-email @db {:email "svet@ttt.com"})
+  (let [{:keys [id]} (authors/get-author-by-email @db {:email "svet@ttt.com"})]
+    (authors/generic-update @db {:id id :updates {:name "Svetlana"}})))
+
+(comment
+  (authors/clj-expr-single-sqlvec {:cols ["email" "name"]})
+  (authors/clj-expr-single @db {:cols ["email" "name"] :id 1})
+  (get-author-by-email @db {:email "svet@ttt.com"}))
+
+(comment
+  (authors/get-all-authors @db))
+
 
