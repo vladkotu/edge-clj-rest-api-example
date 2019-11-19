b89f9c7b584dc47f7bdb1a8fe854fdc073102c2d
Vlad Kotu <vladimir.buhuru@gmail.com>
2019-11-19 20:49:00 +0200

    add dotenv emacs initialisation to dir locals
    despite it not work for eshell

--- wiz.blog.api/.dir-locals.el
+++ wiz.blog.api/.dir-locals.el
@@ -2,4 +2,5 @@
   (cider-ns-refresh-before-fn . "dev-extras/suspend")
   (cider-ns-refresh-after-fn  . "dev-extras/resume")
   (cider-repl-init-code . ("(dev)"))
-  (cider-clojure-cli-global-options . "-A:dev")))
+  (cider-clojure-cli-global-options . "-A:dev")
+  (eval . (proj-dotenv))))
