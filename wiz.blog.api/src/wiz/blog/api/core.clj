228e88246fb9762ccde6d62cf2d595e444f5e240
Vlad Kotu <vladimir.buhuru@gmail.com>
2019-11-21 19:48:56 +0200

    simplify route handlers by using composite integrant keys

--- wiz.blog.api/src/wiz/blog/api/core.clj
+++ wiz.blog.api/src/wiz/blog/api/core.clj
@@ -3,8 +3,7 @@
    [clojure.tools.logging :as log]
    [yada.yada :as yada]
    [yada.swagger :as swagger]
-   [wiz.blog.api.db.books :as books]
-   [wiz.blog.api.db.authors :as authors]
+   [wiz.blog.api.db :as db]
    [integrant.core :as ig]))
 
 (defn pps [x] (with-out-str (clojure.pprint/pprint x)))
@@ -18,29 +17,24 @@
   [_ x]
   (string-resource x))
 
-(defn books [opts db]
-  (yada/resource
-   (merge
-    opts
-    {:methods
-     {:get
-      {:response
-       (fn [ctx]
-         (let [result (books/select-all db)]
-           (case (yada/content-type ctx)
-             "text/plain" (with-out-str (clojure.pprint/pprint result))
-             result)))}}})))
+(def def-opts {:produces   [{:media-type
+                             #{"text/plain;q=0.9"
+                               "application/edn;q=0.7"
+                               "application/json;q=0.8"}}]
+               :properties {:last-modified (new java.util.Date)}})
 
-(defn book [opts db]
+(defmethod ig/init-key ::item-blob
+  [[_ id] {:keys [:wiz.blog.api.db/db-spec :resource/options]}]
   (yada/resource
    (merge
-    opts
+    def-opts
+    options
     {:methods
      {:get
       {:response
        (fn [ctx]
-         (let [id     (-> ctx :parameters :path :id)
-               data   (books/select-by-id db {:id (read-string id)})
+         (let [data   (db/select {:entity (keyword (name id))
+                                  :id (read-string (-> ctx :parameters :path :id))})
                result (if (nil? data)
                         (-> (:response ctx)
                             (assoc :status 404)
@@ -50,70 +44,21 @@
              "text/plain" (with-out-str (clojure.pprint/pprint result))
              result)))}}})))
 
-(defn authors [opts db]
+(defmethod ig/init-key ::items-list
+  [[_ id] {:keys [:wiz.blog.api.db/db-spec :resource/options]}]
   (yada/resource
    (merge
-    opts
-    {:methods
+    def-opts
+    options
+    {:id id
+     :methods
      {:get
       {:response
        (fn [ctx]
-         (let [result (authors/select-all db)]
+         (let [result (db/select {:entity (keyword (name id)) :db-spec db-spec})]
            (case (yada/content-type ctx)
              "text/plain" (with-out-str (clojure.pprint/pprint result))
              result)))}}})))
-
-(defn author [opts db]
-  (yada/resource
-   (merge
-    opts
-    {:methods
-     {:get
-      {:response
-       (fn [ctx]
-         (let [id     (-> ctx :parameters :path :id)
-               data   (authors/select-by-id db {:id (read-string id)})
-               result (if (nil? data)
-                        (-> (:response ctx)
-                            (assoc :status 404)
-                            (assoc :body {:message "Not found"}))
-                        data)]
-           (case (yada/content-type ctx)
-             "text/plain" (with-out-str (clojure.pprint/pprint result))
-             result)))}}})))
-
-(defn defaults [opts]
-  (merge
-   {:produces   [{:media-type
-                  #{"text/plain;q=0.9"
-                    "application/edn;q=0.7"
-                    "application/json;q=0.8"}}]
-    :properties {:last-modified (new java.util.Date)}}
-   opts))
-
-(defmethod ig/init-key ::books
-  [_ db]
-  (-> {:id ::books :description "List of Books"}
-      (defaults)
-      (books db)))
-
-(defmethod ig/init-key ::book
-  [_ db]
-  (-> {:id ::book :description "Full book info"}
-      (defaults)
-      (book db)))
-
-(defmethod ig/init-key ::authors
-  [_ db]
-  (-> {:id ::books :description "List of Books"}
-      (defaults)
-      (authors db)))
-
-(defmethod ig/init-key ::author
-  [_ db]
-  (-> {:id ::book :description "Full book info"}
-      (defaults)
-      (author db)))
 
 (defmethod ig/init-key ::routes
   [_ {:keys [routes port basePath]}]
--- wiz.blog.api/src/wiz/blog/api/db.clj
+++ wiz.blog.api/src/wiz/blog/api/db.clj
@@ -2,12 +2,33 @@
   (:require
    [integrant.core :as ig]
    [clojure.tools.logging :as log]
+
+   [wiz.blog.api.db.books :as books]
    [wiz.blog.api.db.authors :as authors]))
 
-(def db (atom nil))
+(def db-conn (atom nil))
 
 (defmethod ig/init-key ::db-spec
   [_ opts]
-  (log/info "Initialize db")
-  (reset! db opts))
+  (log/info "Initialize db-conn")
+  (reset! db-conn opts))
 
+(defmulti select :entity)
+
+(defmethod select :books
+  [{:keys [db-spec]}]
+  (->> db-spec
+       books/select-all
+       (sort-by :title)))
+
+(defmethod select :book
+  [{:keys [id]}]
+  (authors/select-by-id @db-conn id))
+
+(defmethod select :authors
+  [{:keys [db-spec]}]
+  (authors/select-all db-spec))
+
+(defmethod select :author
+  [{:keys [db-spec]}]
+  (authors/select-all db-spec))
