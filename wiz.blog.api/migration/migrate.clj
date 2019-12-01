ba4b03293a09c29c59d1d3264067290b4c45011b
Vlad Kotu <vladimir.buhuru@gmail.com>
2019-12-01 13:47:56 +0200

    update documentation on how to run system in different modes

--- wiz.blog.api/migration/migrate.clj
+++ wiz.blog.api/migration/migrate.clj
@@ -1,4 +1,4 @@
-(ns revolt-migrations
+(ns migrate
   (:require
    [dev-extras :refer :all]
    [clojure.tools.logging :as log]
@@ -16,7 +16,7 @@
 
 (defn- task [opts]
   (pre-fly-check)
-  ((ns-resolve 'revolt-migrations (symbol @ready-to-fly)) opts))
+  ((ns-resolve *ns* (symbol @ready-to-fly)) opts))
 
 (defn info
   "Flyway info: show statuses of all available migrations"
@@ -27,7 +27,3 @@
   "Flyway migrate: show statuses of all available migrations"
   ([] (migrate {}))
   ([opts] (task (merge opts {:action :migrate}))))
-
-(defn -main [& args]
-  (log/info "-----   MY MAIN")
-  (println "[Mig] " args))
